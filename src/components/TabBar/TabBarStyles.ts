da4014896ba7148c0a79445564b306409330d493
knochenmark <marktodd.hl@gmail.com>
2018-10-04 14:51:14 +0200

    Adjust tab styles

--- src/components/TabBar/TabBarStyles.ts
+++ src/components/TabBar/TabBarStyles.ts
@@ -3,7 +3,7 @@ import { style } from 'typestyle';
 export const tabBarStyle = style({
   alignItems: 'center',
   display: 'flex',
-  justifyContent: 'center',
+  justifyContent: 'flex-start',
 });
 
 export const tabBarItemStyle = style({
@@ -13,5 +13,5 @@ export const tabBarItemStyle = style({
     }
   },
   cursor: 'pointer',
-  margin: '0 1rem',
+  marginRight: '1rem',
 });
