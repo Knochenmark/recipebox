ab6356c444d6980e56b4165cee9e08142fed22aa
knochenmark <marktodd.hl@gmail.com>
2018-10-08 14:54:07 +0200

    Add basic styling to form

--- src/components/RecipeForm/RecipeFormStyles.ts
+++ src/components/RecipeForm/RecipeFormStyles.ts
@@ -6,3 +6,19 @@ export const buttonWrapperStyle = style({
   justifyContent: 'center',
   marginTop: 'auto',
 });
+
+export const recipeFormContentStyle = style({
+  backgroundColor: 'rgba(255,255,255,0.75)',
+  borderRadius: '24px',
+  color: '#666',
+  height: '100%',
+  padding: '1rem',
+  position: 'relative',
+  width: '100%',
+});
+
+export const formStyle = style({
+  alignItems: 'flex-start',
+  display: 'flex',
+  flexDirection: 'column',
+})
