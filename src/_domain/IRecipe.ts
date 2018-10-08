de867ae73a01ce0d701634f2becc31146469d99f
knochenmark <marktodd.hl@gmail.com>
2018-10-08 14:53:13 +0200

    Remove optional flag from imageUrl

--- src/_domain/IRecipe.ts
+++ src/_domain/IRecipe.ts
@@ -2,7 +2,7 @@ import { Difficulty } from './Difficulty';
 
 export interface IRecipe {
   name: string;
-  imageUrl?: string;
+  imageUrl: string;
   isBookmarked: boolean;
   preparationTime: number;
   cookingTime: number;
