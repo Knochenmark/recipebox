9b5e08204029afed7afc24168869a73200cc3bc2
knochenmark <marktodd.hl@gmail.com>
2018-10-10 13:17:11 +0200

    Add formik form

---
+++ src/components/RecipeForm/RecipeValidationSchema.ts
@@ -0,0 +1,15 @@
+import * as Yup from 'yup';
+
+export const RecipeValidationSchema = Yup.object().shape({
+  imageUrl: Yup.string(),
+  name: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  preparationTime: Yup.string()
+    .required('Required')
+    .matches(/^[0-9]*$/, 'Must be a number'),
+  cookingTime: Yup.string()
+    .required('Required')
+    .matches(/^[0-9]*$/, 'Must be a number'),
+});
