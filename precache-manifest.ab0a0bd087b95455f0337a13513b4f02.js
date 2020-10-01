92bb0822fa9502a06ead2608ae971070fb2bcd6a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-01 19:37:50 +0000

    Deploy Knochenmark/recipebox to github.com/Knochenmark/recipebox.git:gh-pages

---
+++ precache-manifest.ab0a0bd087b95455f0337a13513b4f02.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6afe26c19aaa28d6dc870e0b9012673b",
+    "url": "/recipebox/index.html"
+  },
+  {
+    "revision": "941abb2561daeb74ab8e",
+    "url": "/recipebox/static/css/main.ce1dc18d.chunk.css"
+  },
+  {
+    "revision": "cab77ef28abb176439b4",
+    "url": "/recipebox/static/js/2.9ba9f0a4.chunk.js"
+  },
+  {
+    "revision": "70d36ae31987e7aa8f351aa74965fa05",
+    "url": "/recipebox/static/js/2.9ba9f0a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "941abb2561daeb74ab8e",
+    "url": "/recipebox/static/js/main.d5ab5fbc.chunk.js"
+  },
+  {
+    "revision": "d98f5347315ae8181675",
+    "url": "/recipebox/static/js/runtime-main.516a01f5.js"
+  },
+  {
+    "revision": "6427e47bb50f976149be8b08424e06ce",
+    "url": "/recipebox/static/media/cooking01.6427e47b.jpg"
+  },
+  {
+    "revision": "699b9b104d5118b97464e7747eed443f",
+    "url": "/recipebox/static/media/cooking04.699b9b10.jpg"
+  }
+]);
